6eb91a2fb3666ef1c58c4450a1e9714d8bfccfa3
Simon Newton <simonanewton@gmail.com>
2020-03-27 13:38:57 -0400

    1.5 added question cycling functionality

--- questions.js
+++ questions.js
@@ -1,25 +1,25 @@
 const questions = [
     {
         title: "Question One:",
-        question: "What is the answer to this question?",
+        question: "What is the answer to question 1?",
         choices: ["Answer 1", "Answer 2", "Answer3", "Answer 4"],
         answer: "Answer 1",
     },
     {
         title: "Question Two:",
-        question: "What is the answer to this question?",
+        question: "What is the answer to question 2?",
         choices: ["Answer 1", "Answer 2", "Answer3", "Answer 4"],
         answer: "Answer 2",
     },
     {
         title: "Question Three:",
-        question: "What is the answer to this question?",
+        question: "What is the answer to question 3?",
         choices: ["Answer 1", "Answer 2", "Answer3", "Answer 4"],
         answer: "Answer 3",
     },
     {
         title: "Question Four:",
-        question: "What is the answer to this question?",
+        question: "What is the answer to question 4?",
         choices: ["Answer 1", "Answer 2", "Answer3", "Answer 4"],
         answer: "Answer 4",
     }
--- script.js
+++ script.js
@@ -1,178 +1,186 @@
-// initializes button variables
-var btnStart = document.querySelector(".btnStart");
-var btnNext = document.querySelector(".btnNext");
-var btnSubmit = document.querySelector(".btnSubmit");
-var btnStartOver = document.querySelector(".btnRestart");
-var btnClearScores = document.querySelector(".btnClearScores");
+//--------------------------------------------------------------------------------------------------------------------
 
-// variables for each page div section
-var startDiv = document.querySelector(".startPage");
-var questionOneDiv = document.querySelector(".questionOne");
-var questionTwoDiv = document.querySelector(".questionTwo");
-var questionThreeDiv = document.querySelector(".questionThree");
-var endDiv = document.querySelector(".endPage");
-var highscoresDiv = document.querySelector(".highscores");
+// initialize button variables
+var btnStartQuiz = document.querySelector(".btnStartQuiz");
+var btnNextQuestion = document.querySelector(".btnNext");
+var btnFinishQuiz = document.querySelector(".btnFinishQuiz");
+
+// variable for section divs
+var startPageDiv = document.querySelector(".startPageDiv");
+var questionDiv = document.querySelector(".questionDiv");
+var choicesDiv = document.querySelector(".choicesDiv");
+var buttonDiv = document.querySelector(".buttonDiv");
+var endPageDiv = document.querySelector(".endPageDiv");
 
 // initializes the quiz timer
-var timer = document.querySelector(".timer");
+var timer = document.querySelector("#timer");
 
 // initializes the progress bar variables
 var progressBar = document.querySelector(".progress-bar");
 var currentProgress = 0;
 
-// initializes user score
+// initializes user score elements
 var score = 0;
+var userScore = document.querySelector("#userScore");
 
-//---------------------------------------------------------------
+// initialize question array index
+var currentQuestionIndex = 0;
+// var lastQuestionIndex = questions.length;
 
-// starts the quiz timer
+//--------------------------------------------------------------------------------------------------------------------
+
 function startTimer() {
     var startTime = 60;
     setInterval(function () {
         if (startTime > 0) startTime--;
         timer.textContent = "Timer: " + startTime + "s";
     }, 1000);
-    //Place the question grabbing function 
-    //questionGrab()
 }
 
-// resets the value of the progress bar
+function toggleShowElement(div) {
+    if (div.getAttribute("style") === "display: none;") div.removeAttribute("style");
+    else div.setAttribute("style", "display: none;");
+}
+
+function updateProgress() {
+    var interval = 25;
+    if (currentProgress < 100) currentProgress += interval;
+    progressBar.setAttribute("style", `width: ${currentProgress}%`);
+    progressBar.setAttribute("aria-valuenow", currentProgress);
+}
+
 function resetProgress() {
     currentProgress = 0;
-    progressBar.setAttribute("style", "width: " + currentProgress + "%");
+    progressBar.setAttribute("style", `width: ${currentProgress}%`);
     progressBar.setAttribute("aria-valuenow", currentProgress);
 }
 
-// updates the value of the progress bar
-function updateProgress() {
-    var interval = 20;
-    if (currentProgress < 100) currentProgress += interval;
-    progressBar.setAttribute("style", "width: " + currentProgress + "%");
-    progressBar.setAttribute("aria-valuenow", currentProgress);
-}
-
-// toggles the visibility of a given element
-function toggleShowDiv(div) {
-    if (div.style.display === "none") div.style.display = "block";
-    else if (div.style.display === "block") div.style.display = "none";
-}
-
-// adds start button functionality
-btnStart.addEventListener("click", function () {
-    // start timer
-    startTimer();
-
-    // update progress bar
-    updateProgress();
-
-    // retrieve and display questions
-    getQuestions();
-
-    // hide start page
-    toggleShowDiv(startDiv);
-
-    // show first question
-    toggleShowDiv(questionOneDiv);
-});
-
-//---------------------------------------------------------------
-
-// // adds next button functionality
-// btnNext.addEventListener("click", function() {
-
-//     // hide current question
-
-//     // update progress bar
-
-//     // if not last question, show next question
-
-//     // else if last question, show end page
-// });
-
-// // adds submit button functionality
-// btnSubmit.addEventListener("click", function() {
-//     // create variable for initals
-
-//     // save initials and score to localStorage
-
-//     // show highscores page
-// });
-
-// // adds start over button functionality
-// btnStartOver.addEventListener("click", function() {
-//     // hide highscores page
-
-//     // show start page
-// });
-
-// // adds clear scores button functionality
-// btnClearScores.addEventListener("click", function() {
-//     // clear all scores from highscores list
-// });
-
-//-----------------------------------------------
-
-
-// TO DO:
-// add toggleShowDiv functionality
-// add stopTimer functionality, stop the timer if all questions have been answered
-// set next button as disabled until an answer has been chose for that question
-// add updateProgress functionality
-
-
-
-
-
-
-
-
-function getQuestions() {
-    // initialize question array index
-    var currentQuestionIndex = 0;
-
+function loadQuestion() {
     // gets the current question object from the questions array
     var currentQuestion = questions[currentQuestionIndex];
 
     // takes the title value from the current question object and sets the question title to its value
-    var questionTitle = document.getElementById("title");
+    var questionTitle = document.getElementById("questionTitle");
     questionTitle.textContent = currentQuestion.title;
-    
+
     // takes the question value from the current question object and sets the question text to its value
-    var questionText = document.getElementById("question");
+    var questionText = document.getElementById("questionPrompt");
     questionText.textContent = currentQuestion.question;
 
     // iterate through question choices and display them
     currentQuestion.choices.forEach(choice => {
+        // create and set attributes for each choice button
+        var choiceEl = document.createElement("button");
+        choiceEl.setAttribute("class", "btn btn-outline-primary rounded my-2");
+        choiceEl.setAttribute("type", "button");
+        // choiceEl.setAttribute("choice", choice);
+
+        // add click functionality to the choice button
         // ???
+
+        // set the choice button text content
+        choiceEl.textContent = choice;
+
+        // display the choice element
+        choicesDiv.appendChild(choiceEl);
     });
+
+    if (currentQuestionIndex < (questions.length - 1)) {
+        // create next button
+        var nextButtonEl = document.createElement("button");
+        nextButtonEl.setAttribute("class", "btn btn-primary w-25 rounded btnNext");
+        nextButtonEl.setAttribute("type", "button");
+        nextButtonEl.textContent = "Next Question";
+        buttonDiv.appendChild(nextButtonEl);
+    }
+    else {
+        // create finish quiz button
+        var finishQuizButtonEl = document.createElement("button");
+        finishQuizButtonEl.setAttribute("class", "btn btn-primary w-25 rounded btnFinishQuiz");
+        finishQuizButtonEl.setAttribute("type", "button");
+        finishQuizButtonEl.textContent = "Finish Quiz";
+        buttonDiv.appendChild(finishQuizButtonEl);
+    }
 }
 
+//--------------------------------------------------------------------------------------------------------------------
 
-// const currentQuestionIndex = //stands for the index of the question on the page 
-// function getQuestion() {
-//     // get current question object from array
-//     var currentQuestion = questions[currentQuestionIndex];
-  
-//     // update title with current question
-//     var titleEl = document.getElementById("title");
-//     titleEl.textContent = currentQuestion.title;
-  
-//     // clear out any old question choices
-//     choicesEl.innerHTML = "";
-  
-//     // loop over choices
-//     currentQuestion.choices.forEach(function(choice, i) {
-//       // create new button for each choice
-//       var choiceNode = document.createElement("button");
-//       choiceNode.setAttribute("class", "choice");
-//       choiceNode.setAttribute("value", choice);
-  
-//       choiceNode.textContent = i + 1 + ". " + choice;
-  
-//       // attach click event listener to each choice
-//       choiceNode.onclick = questionClick;
-  
-//       // display on the page
-//       choicesEl.appendChild(choiceNode);
-//     });
-//   }
+questionDiv.addEventListener("click", function () {
+
+    if (event.target && event.target.matches("button.btnNext")) {
+        // calculate score
+        // ???
+        
+        // clear current question content
+        choicesDiv.innerHTML = "";
+        buttonDiv.innerHTML = "";
+
+        // update currentQuestionIndex
+        currentQuestionIndex++;
+        // console.log(lastQuestionIndex);
+
+        // load the next question
+        loadQuestion();
+
+        // update progressBar
+        updateProgress();
+    }
+
+    if (event.target && event.target.matches("button.btnFinishQuiz")) {
+        // calculate score
+        userScore.textContent = `Your score is: ${score}`;
+
+        // hide the question div
+        toggleShowElement(questionDiv);
+
+        // show the end page div
+        toggleShowElement(endPageDiv);
+    }
+
+});
+
+btnStartQuiz.addEventListener("click", function () {
+    // start timer
+    startTimer();
+
+    // hides the start page
+    toggleShowElement(startPageDiv);
+
+    // load the first question
+    loadQuestion();
+
+    // show the first question
+    toggleShowElement(questionDiv);
+
+    // update the progress bar
+    updateProgress();
+});
+
+//--------------------------------------------------------------------------------------------------------------------
+
+// upon startQuiz button clicked, start timer
+// hide startPage
+
+// update progressBar
+// show questionPage
+// if not the last question, show nextButton
+// upon nextButton clicked, get user choice, compare choice to answer value, modify score, modify timer
+// hide current question
+// show next question
+
+// repeat until last question
+
+// on last question, show finshQuiz button
+// upon finishQuiz button clicked, stop timer, add timer to score
+
+// show endPage, show score, show input for initials
+// upon submit button clicked, store initals and score in localStorage
+// hide endPage
+
+// show highscoresPage
+// show all highscores with initials
+// upon clearHighscores button clicked, clear all highscores from localStorage
+
+// upon restartQuiz button clicked, reset score, reset timer, reset currentQuestionIndex, reset progressBar
+// hide highscoresPage
+// show startPage
